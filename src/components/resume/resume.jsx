88c9a4c83a836010b968463d3541398cc80493d2
huimintoh <hmintoh@gmail.com>
2018-11-24 17:51:15 +0800

    to add resume component skeleton

---
+++ src/components/resume/resume.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./resume.module.css";
+
+const Resume = () => {
+  return (
+    <div className="light">
+      <p>Resume pending implementation</p>
+    </div>
+  );
+};
+
+export default Resume;
