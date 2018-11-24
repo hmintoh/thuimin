10a3b314724b349b85d2f08f50cfc7e41d6b72cd
huimintoh <hmintoh@gmail.com>
2018-11-24 17:50:27 +0800

    styling for footer component

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,9 +3,9 @@ import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <div>
-      <p>Footer pending implementation</p>
-    </div>
+    <footer>
+      <p>copyright &copy; 2018 thm. all rights reserved</p>
+    </footer>
   );
 };
 
