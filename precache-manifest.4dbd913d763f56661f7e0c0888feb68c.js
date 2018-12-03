6ef530f08a4f0a0d8f6ac2d0c4fcef3c3cd498b8
huimintoh <hmintoh@gmail.com>
2018-12-03 14:18:31 +0800

    Updates

---
+++ precache-manifest.4dbd913d763f56661f7e0c0888feb68c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "09cca4eb9473cc00dbc5c171c232257f",
+    "url": "/thuimin/static/media/jumbotron.09cca4eb.jpg"
+  },
+  {
+    "revision": "6beb319cceb1faca872c9552aeb1e43a",
+    "url": "/thuimin/static/media/avatar.6beb319c.jpeg"
+  },
+  {
+    "revision": "bd8200f36875ce91c1665c031557a05b",
+    "url": "/thuimin/static/media/Linkedin.bd8200f3.png"
+  },
+  {
+    "revision": "b7f43fab2eaf1b100b6196dee8f42a31",
+    "url": "/thuimin/static/media/Gmail.b7f43fab.png"
+  },
+  {
+    "revision": "e271dfbed96627660dc958765833ba53",
+    "url": "/thuimin/static/media/Github.e271dfbe.png"
+  },
+  {
+    "revision": "459af4c3d76fcc9536ff",
+    "url": "/thuimin/static/js/runtime~main.459af4c3.js"
+  },
+  {
+    "revision": "7b3d554a064c09edc58a",
+    "url": "/thuimin/static/js/main.7b3d554a.chunk.js"
+  },
+  {
+    "revision": "961636b6d8b64b05f49e",
+    "url": "/thuimin/static/js/1.961636b6.chunk.js"
+  },
+  {
+    "revision": "7b3d554a064c09edc58a",
+    "url": "/thuimin/static/css/main.08423c17.chunk.css"
+  },
+  {
+    "revision": "6d6afd5e7e3cb593404cd18b8b5841ce",
+    "url": "/thuimin/index.html"
+  }
+];
